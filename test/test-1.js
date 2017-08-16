38bd64cfbfae5ba393bd37bc4163ae04f8472f07
shgtkshruch <shgtk.shruch@gmail.com>
2017-08-16 21:28:21 +0900

    Initial commit

---
+++ test/test-1.js
@@ -0,0 +1,63 @@
+const webdriver = require('selenium-webdriver');
+const test = require('selenium-webdriver/testing');
+const assert = require('assert');
+const By = webdriver.By;
+const until = webdriver.until;
+const fs = require('fs');
+
+let t;
+let i = 1;
+let taskName;
+const extension = 'png';
+
+test.describe('Google検索', () => {
+  let driver;
+
+  test.before(() => {
+    driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
+    driver.manage().timeouts().implicitlyWait(3000);
+  });
+
+  test.after(() => {
+    driver.quit();
+  });
+
+  // テスト後にスクリーンショットを取る
+  test.afterEach(done => {
+    var filename = taskName;
+    driver.takeScreenshot().then(data => {
+      fs.writeFile(
+        `${i}.${filename}.${extension}`,
+        data.replace(/^data:image\/png;base64,/,''),
+        'base64',
+        (err) => {
+          if(err) throw err;
+          i++
+          done();
+        });
+    });
+  });
+
+  test.it('正しいタイトルが表示される', done => {
+    taskName = '検索'
+    driver.get('http://www.google.co.jp/');
+    driver.findElement(By.name('q')).sendKeys('webdriver');
+    driver.findElement(By.id('tsf')).submit();
+    driver.wait(until.titleIs('webdriver - Google 検索'), 1000)
+    .then(() => {
+      return driver.getTitle();
+    })
+    .then(title => {
+      t = title
+      assert.equal(title, 'webdriver - Google 検索');
+      done();
+    });
+  });
+});
+
+test.describe('テスト間の連携(in file)', () => {
+  taskName = '変数'
+  test.it('変数を受け取る', () => {
+    assert(t, 'webdriver - Google 検索');
+  });
+});
---
+++ test/test.js
@@ -0,0 +1,2 @@
+require('./test-1.js');
+require('./test-2.js');
